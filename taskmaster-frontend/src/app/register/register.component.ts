eceeb6e4645d5501f1bee411f62decbcfb04acb8
DKichukov <dzhamal.kichukov@gmail.com>
2025-01-26 21:09:43 +0200

    implement registration

---
+++ taskmaster-frontend/src/app/register/register.component.ts
@@ -0,0 +1,36 @@
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService, RegisterRequest} from '../services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  registerForm: FormGroup
+  errorMessage: string = "";
+
+
+  constructor(private authService: AuthService, private fb: FormBuilder) {
+    this.registerForm = fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required, Validators.minLength(6)],
+      role: ['USER', Validators.required],
+    })
+  }
+ 
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const registerRequest: RegisterRequest = this.registerForm.value;
+      this.authService.register(registerRequest).subscribe();
+    } else {
+      this.errorMessage = 'Please fill in all the required fields';
+    }
+  }
+
+}
